cdc39838bd3dda5535dd01ebe44e3ec04d1b0dbe
Manisso <manis98@live.com>
2017-12-07 21:32:04 +0100

    Add files via upload

---
+++ angryip.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+print('''
+     _                             ___ ____  
+    / \   _ __   __ _ _ __ _   _  |_ _|  _ \ 
+   / _ \ | '_ \ / _` | '__| | | |  | || |_) |
+  / ___ \| | | | (_| | |  | |_| |  | ||  __/ 
+ /_/   \_\_| |_|\__, |_|   \__, | |___|_|    
+                |___/      |___/             
+Install Me For Direct Use
+''')
+
+os.system("sudo su") #Sudo Su
+os.system("pwd") #show Path
+os.system("java -jar AngryIP.jar")
+
